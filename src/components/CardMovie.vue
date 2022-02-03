b05762e1f6a75b67588923e6c2ef44d321b37ae8
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-03 20:13:41 +0100

    Add: poster

--- src/components/CardMovie.vue
+++ src/components/CardMovie.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="card">
-        <div><b>Titolo:</b> {{movie.title}}</div>
-        <div><b>Titolo originale:</b> {{movie.original_title}}</div>
-        <div><b>Lingua:</b> {{movie.original_language}}</div>
-        <div><b>Voto:</b> {{movie.vote_average}}</div>
+        <div class="box-img">
+            <img :src="'https://image.tmdb.org/t/p/original/' + movie.poster_path"/>
+        </div>
+        <p><b>Titolo:</b> {{movie.title}}</p>
+        <p><b>Titolo originale:</b> {{movie.original_title}}</p>
+        <p><b>Lingua:</b> {{movie.original_language}}</p>
+        <p><b>Voto:</b> {{movie.vote_average}}</p>
     </div>
 </template>
 
@@ -21,9 +24,31 @@ export default {
         border:white solid 1px;
         color: white;
         width: 300px;
-        height: 500px;
+        height: 450px;
         margin: 20px 20px;
         padding: 40px 10px;
         font-size: 25px;
+        position: relative;
+        
+        .box-img{
+            height: 450px;
+            width: 298px;
+            position: absolute;
+            top: 0;
+            left: 0;
+
+            img{
+                width: 100%;
+                object-fit: cover;
+            }
+
+            &:hover img{
+                display: none;
+            }
+        }
+        p{
+           line-height: 35px;
+        }
+        
     }
 </style>
