d2ec259ef1ccc8ca409652d027681563f443c9f4
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-03 16:49:47 +0100

    Add: getMovies method

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-   
+     
   </div>
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
 
 
 export default {
   name: 'App',
   components: {
     
+  },
+  data(){
+    return{
+      filteredMovies:[]
+    }
+  },
+  methods:{
+    getMovies(name){
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e5b44d73cf6acb74493cea658f17750e&query=${name}&language=it`).then((list)=>{
+          this.filteredMovies=list.data.results
+      })
+    }
   }
 }
 </script>
